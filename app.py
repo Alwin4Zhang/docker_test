1f74a4d65d4a552706116f76389e1964b2575fa1
Alwin.Zhang <alwin114@hotmail.com>
2021-02-23 11:33:02 +0800

    fix: fix apprun host使得docker image可以外部访问,否则只能ping通但是没有返回

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "hello world"
 
+@app.route("/hello")
+def hello_flask():
+    return {"result":"Hello flask!"}
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0")
     
