54aa962c29b61abcfa6ac5f7a5e0b68df9ce40e3
alwin <alwin114@hotmail.com>
2021-08-16 22:24:03 +0800

    chore: make a change

--- app.py
+++ app.py
@@ -2,26 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
-
 @app.route("/")
 def hello_world():
-	print("update jenkins!")
-    return "Hello Flask!"
+    return "Hello Flask,I am hello_world!"
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
-=======
-@app.route("/")
-def hello():
-    return "hello world"
-
-@app.route("/hello")
-def hello_flask():
-    return {"result":"Hello flask!"}
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0")
-    
->>>>>>> main
