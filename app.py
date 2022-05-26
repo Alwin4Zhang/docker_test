6bcc46a416b28b54d5bc87150dda725168c71dde
Alwin.Zhang <alwin114@hotmail.com>
2022-05-26 11:26:38 +0800

    chore: add test api

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello Flask,I am hello_world!"
 
+@app.route("/hello")
+def hello():
+    return "hello world!"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
