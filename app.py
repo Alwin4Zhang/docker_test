0e18fae9d1380405c8f8122f8701f3b2f4a4f6c3
Alwin.Zhang <alwin114@hotmail.com>
2022-05-27 11:25:14 +0800

    feat: 触发构建

--- app.py
+++ app.py
@@ -14,5 +14,10 @@ def test():
 def test2():
     return "hello test!"
 
+@app.route("/test2")
+def test3():
+    return "hello test2!"
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
